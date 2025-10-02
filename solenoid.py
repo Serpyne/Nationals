1661acdd07d68f2d0fa01ee9272188267388d49e
Serpyne <robbingchewing@gmail.com>
2025-10-02 13:44:18 +1000

    Edited solenoid.py for the most effective timing.

--- solenoid.py
+++ solenoid.py
@@ -1,36 +1,51 @@
-from gpiozero import LED
+from gpiozero import DigitalOutputDevice
 from time import sleep
+from motors_i2c import Motor
+import asyncio
 
 from pathlib import Path
 import json
 
 
+
 class Solenoid:
     def __init__(self, activation_pin: int):
         self.pin = activation_pin
-        self.device = LED(self.pin)
-    def shoot(self):
-        # DECIDE IF THIS FUNCTION IS ASYNC OR BLOCKING.
+        self.device = DigitalOutputDevice(self.pin, active_high=False, initial_value=False)
+        
+    async def shoot(self):
         self.device.on()
-        sleep(0.5)
+        await asyncio.sleep(0.05)
         self.device.off()
+        await asyncio.sleep(0.1)
+
 
 
 # For testing that the kicker won't exceed the maximum power.
 # A pass is when the robot is placed in the backleft corner of the defending goal
 # and kicked into the attacking goal, the ball bounces off the back of the goal and sits in the penalty area.
+
 if __name__ == "__main__":
-    with open(Path(__file__).parent / "config.json", "r") as f:
-        config = json.load(f)
-        f.close()
-        
-    self.utils.switch_left = Button(config["addresses"]["switchLeft"], pull_up=False)
-    self.utils.switch_right = Button(config["addresses"]["switchRight"], pull_up=False)
-    
-    solenoid = Solenoid(17)
-    
-    while True:
-        if self.utils.switch_left.is_pressed or self.utils.switch_right.is_pressed:
-            solenoid.shoot()
+    s = Solenoid(23)
+    m = Motor(0x1B, max_speed = 200_000_000)
+
+    runMotors = False
+    m.set_speed(0, True)
+
+    async def main():
+        while True:
+            if runMotors: m.set_speed(-1, True)
             
-        sleep(0.01)
+            await asyncio.sleep(0.1)
+            input()
+            
+            if runMotors: m.set_speed(0.5, True)
+            await asyncio.sleep(0.15)
+            await s.shoot()
+                
+            await asyncio.sleep(0.1)
+    
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        m.set_speed(0, True)
